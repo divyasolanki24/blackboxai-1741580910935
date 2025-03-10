95d1aff0c2d95c3513cfdb185ec84fed10154dc9
divyasolanki24 <divyatailor18@gmail.com>
2025-03-10 09:59:06 +0530

    Add shop_management_system/lib/models/activity.dart 1741580945479

---
+++ shop_management_system/lib/models/activity.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Activity {
+  final IconData icon;
+  final Color color;
+  final String title;
+  final String subtitle;
+  final String time;
+
+  Activity({
+    required this.icon,
+    required this.color,
+    required this.title,
+    required this.subtitle,
+    required this.time,
+  });
+}
