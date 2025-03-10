12681718d69cc01ba733d7a144543b74c6e3b26e
divyasolanki24 <divyatailor18@gmail.com>
2025-03-10 09:59:11 +0530

    Add shop_management_system/lib/modules/auth/bindings/auth_binding.dart 1741580950730

---
+++ shop_management_system/lib/modules/auth/bindings/auth_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:shop_management_system/modules/auth/controllers/auth_controller.dart';
+
+class AuthBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<AuthController>(
+      () => AuthController(),
+    );
+  }
+}
